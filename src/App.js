5ef5964fe11e386856cc211239ebc72470ca73c5
Vinicius Amaro Cechin <vinicechin@gmail.com>
2021-10-18 00:26:13 +0200

    Basic welcome page implementation
    Add some default text to the welcome page. Modified App component to be a class instead of a function. Added basic navbar to the welcome page. Added basic css.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  renderNavBar() {
+    return (
+      <header class="AppHeader">
+        <nav>
+          <ul class="AppNav">
+            <li class="AppNavItem">
+                Items
+            </li>
+            <li class="AppNavItem">
+                Profile
+            </li>
+            <li class="AppNavItem">
+                Login
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+  renderWelcome() {
+    return (
+      <header className="App-welcome">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p id="app-name">
           0 Waste 2 Life
         </p>
+        <p class="app-welcome">
+          Got something you don't want anymore? Need something but it is too expensive?
+        </p>
+        <p class="app-welcome">
+          Then you are in the right place!
+        </p>
       </header>
-    </div>
-  );
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.renderNavBar()}
+        {this.renderWelcome()}
+      </div>
+    )
+  }
 }
 
 export default App;
