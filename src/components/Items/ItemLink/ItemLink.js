7c4890d62eb08f2ce501291b592ad086052fd699
Vinicius Cechin <vinicechin@gmail.com>
2021-11-30 14:32:38 +0100

    Add images based on url to items display
    Also use card and grid elements to show available items on items page

--- src/components/Items/ItemLink/ItemLink.js
+++ src/components/Items/ItemLink/ItemLink.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 import './ItemLink.css'
 
@@ -6,7 +12,23 @@ class ItemLink extends Component{
   render(){
     return (
       <div>
-          {this.props.information.name}
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia 
+            // square 
+            component="img"
+            src={this.props.information.url} 
+            // imageUrl={this.props.information.url} 
+            alt="Can't load image properly"
+          />
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              {this.props.information.name}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
       </div>
     )
   } 
--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 import ItemLink from './ItemLink/ItemLink';
 
@@ -22,10 +23,14 @@ class Items extends Component{
         <div className="Items">
           <Typography variant="h3" component="div" className="App-title-primary-color" sx={{ mb:2 }}>Items</Typography>
           
-          {items.map((item) => {
-            const { item_id, ...information } = item
-            return <ItemLink key={item_id} information={information} />
-          })}
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {items.map((item) => {
+              const { item_id, ...information } = item
+              return <Grid item xs={2} sm={4} md={4} key={item_id}>
+                <ItemLink key={item_id} information={information} />
+              </Grid>
+            })}
+          </Grid>
         </div>
       </Box>
     )
